c6329b7d6ce2efed92c7fb4cde93dd4d0cb64f09
Varun Ramesh <varun.ceg.95@gmail.com>
2018-04-29 17:24:36 -0500

    Updated merger

--- Stage4/Code/data_merger.py
+++ Stage4/Code/data_merger.py
@@ -1,9 +1,9 @@
 import numpy as np
 import pandas as pd
+import pdb
 
-data_columns = ['movie_name']
-# 'movie_title', 'movie_rating',
-#                 'movie_genre', 'movie_year', 'movie_directors',
+data_columns = ['movie_name', 'movie_rating', 'movie_year']
+#                 'movie_genre', , 'movie_directors',
 #                 'movie_actors']
 
 candidate_dataframe = pd.read_csv('../data/Candidate_Matches.csv')
@@ -13,8 +13,7 @@ predicted_dataframe = pd.read_csv('../data/Predicted.csv')
 possible_matches = len(predicted_dataframe[
                         (predicted_dataframe['predicted'] == 1)])
 
-possible_range = range(possible_matches)
-
+possible_ranges = range(possible_matches)
 # Get new dataframe
 id_count = 0
 
@@ -22,7 +21,7 @@ id_count = 0
 added_movies = []
 
 # Create new dataframe
-movies = pd.DataFrame(index = possible_range, columns=data_columns)
+movies = pd.DataFrame(index = possible_ranges,columns=data_columns)
 
 # Iterate through all the predictions_dataframe
 for i, row in predicted_dataframe.iterrows():
@@ -34,12 +33,48 @@ for i, row in predicted_dataframe.iterrows():
     if int(row['predicted']) == 1:
         # Check if one of the ids is already in added_movies
         if l_id not in added_movies and r_id not in added_movies:
-            # adding movie title
-            # equal movie names
-            # if candidate_dataframe[_id]['ltable_Title'] == candidate_dataframe[_id]['rtable_Title']:
-            movies.iloc[id_count]['movie_name'] = candidate_dataframe.loc[candidate_dataframe['ltable_id'] == l_id]['ltable_Title']
+
+            # adding movie title - Always take imdb movie title
+            movie_name_series = candidate_dataframe.loc[candidate_dataframe['rtable_id'] == r_id]['rtable_Title']
+            movie_name_str = str(movie_name_series[0])
+
+            # Get average rating - (Filmcrave * 2.5 + imdb)/2
+            filmcrave_rating_series = candidate_dataframe.loc[candidate_dataframe['ltable_id'] == l_id]['ltable_Overall Rating']
+            imdb_rating_series = candidate_dataframe.loc[candidate_dataframe['rtable_id'] == r_id]['rtable_Overall Rating']
+
+            # Some cleaning stuff
+            filmcrave_rating_list = str(filmcrave_rating_series[0]).split('/')
+            filmcrave_rating = float(filmcrave_rating_list[0])
+            imdb_rating = float(imdb_rating_series[0])
+            average_rating = (filmcrave_rating * 2.5 + imdb_rating) / 2
+
+            # Get movie year. Always take IMDB year.
+            movie_year_series = candidate_dataframe.loc[candidate_dataframe['rtable_id'] == r_id]['rtable_Year']
+            movie_year = int(movie_year_series[0])
+
+            # Get Movie Genres - Take union of both genres
+            filmcrave_genre_series = candidate_dataframe.loc[candidate_dataframe['ltable_id'] == l_id]['ltable_Genre']
+            imdb_genre_series = candidate_dataframe.loc[candidate_dataframe['rtable_id'] == r_id]['rtable_Genre']
+
+            # Getting individual genre fields
+            filmcrave_genres = str(filmcrave_genre_series[0]).split('/')
+            imdb_genres = str(imdb_genre_series[0]).split(',')
+            genre_set = set()
+
+            # cleaning genre and getting union
+            for val in imdb_genres:
+                val = str(val).strip()
+                genre_set.add(val)
+
+            for val in filmcrave_genres:
+                val = str(val).strip()
+                genre_set.add(val)
+
+            movie_genre_list = list(genre_set)
+            movie_genres = ','.join(movie_genre_list)
+
+
             added_movies.append(l_id)
             added_movies.append(r_id)
+            break
             id_count += 1
-
-print(movies)
